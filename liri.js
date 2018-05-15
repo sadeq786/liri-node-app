ff650bbebaba5552273bd69c51a1a909961f7616
sadeq786 <sadeq786@gmail.com>
2018-05-15 15:39:09 -0500

    Fixed OMDBP API info and do-what-it-says function

--- liri.js
+++ liri.js
@@ -10,24 +10,29 @@ var spotify = new spotify(keys.spotify);
 var client = new twitter(keys.twitter);
 
 var command = process.argv[2];
+var title = process.argv[3];
 
+runSwitch(command);
 
-switch (command) {
-    case 'my-tweets':
-        myTweets();
-        break;
-    case 'spotify-this-song':
-        spotifyTrack();
-        break;
-    case 'movie-this':
-        movie();
-        break;
-    case 'do-what-it-says':
-        doWhatItSays();
-        break;
-    default:
-        console.log("Wrong command, please try again !");
+function runSwitch(command) {
+    switch (command) {
+        case 'my-tweets':
+            myTweets();
+            break;
+        case 'spotify-this-song':
+            spotifyTrack();
+            break;
+        case 'movie-this':
+            movie();
+            break;
+        case 'do-what-it-says':
+            doWhatItSays();
+            break;
+        default:
+            console.log("Wrong command, please try again !");
+    }
 }
+
 
 function myTweets() {
     var params =
@@ -45,11 +50,11 @@ function myTweets() {
 }
 
 function spotifyTrack() {
-    
-    if (!process.argv[3]) {
-        process.argv[3] = 'The Sign';
+
+    if (!title) {
+        title = 'The Sign';
     }
-    spotify.search({ type: 'track', query: process.argv[3] }, function (err, data) {
+    spotify.search({ type: 'track', query: title }, function (err, data) {
         if (err) {
             return console.log('Error occurred: ' + err);
         }
@@ -66,35 +71,34 @@ function spotifyTrack() {
 
 function movie() {
 
-    if (!process.argv[3]) {
-        process.argv[3] = 'Mr. Nobody';
+    if (!title) {
+        title = 'Mr. Nobody';
     }
-    var movieName = process.argv[3];
-    request('http://www.omdbapi.com/?apikey=trilogy&s=' + movieName, function (error, response, body) {
-        // console.log('error:', error); // Print the error if one occurred
-        // console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
-        ourData = JSON.parse(body);
-        for (var i = 0 ; i < 5 ; i++) {
-        console.log("Movie Title : ", ourData.Search[i].Title);
-        console.log("Year of Release: ", ourData.Search[i].Year);
-        console.log();
-        }
+    var movieName = title;
+    request('http://www.omdbapi.com/?apikey=trilogy&t=' + movieName, function (error, response, body) {
+        var ourData = JSON.parse(body);
+            console.log();
+            console.log("Title: ", ourData.Title);
+            console.log("Year: ", ourData.Year);
+            console.log("imdbRating: ", ourData.imdbRating);
+            console.log("Rotten Tomatoes Score: ", ourData.Ratings[1].Value)
+            console.log("Country: ", ourData.Country);
+            console.log("Language: ", ourData.Language);
+            console.log("Plot: ", ourData.Plot);
+            console.log("Actors in the Movie: ", ourData.Actors);
+            console.log();
+        
     });
 }
 
-function doWhatItSays () {
-    fs.readFile('random.txt', function(err, data) {
-        //get index of ','. this is what separates the command from the name of the song/movie
-        // str = data;
-        // console.log(data);
-        var comma = data.indexOf(',');
-        // console.log(comma);
-        var fileCommand = data.substring(1 , comma);
-        // console.log(fileCommand); 
-        var songName = data.substring(comma+2 , str.length-1);
-        process.argv[3] = songName;
-        command = fileCommand;
+function doWhatItSays() {
+    fs.readFile('random.txt', "UTF-8", function (err, data) {
+        if (err) {
+            return console.log(err);
+        }
 
-
+        title = data.split(",")[1];
+        command = data.split(",")[0];
+        runSwitch(command);
     });
 }
